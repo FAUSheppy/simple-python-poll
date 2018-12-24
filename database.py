0eb63339965a695a786fc6609c51fabf0ac2dd0f
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2018-12-24 16:44:37 +0100

    initial

---
+++ database.py
@@ -0,0 +1,12 @@
+# init
+    # db-poll:    name | options | tokens? | openresults? | date
+    # db-options: name+option | count
+    # db-tokens   token | name | used | option(s)
+
+# new vote
+    # check token
+    # increment count +1
+
+# check token
+    # check if token still db
+    # return options if already used
---
+++ frontend.py
@@ -0,0 +1,21 @@
+# main page
+#    center: new poll
+# create poll page:
+#    - options already added - delete button -
+#    - new option -
+#    - cancle --------------------- submitt --
+#    - use tokens [ ]
+# poll create page:
+#    center: link to poll (copyable)
+# ask token page:
+#    center: enter token
+# poll page:
+#    - option 1 [ ]
+#    - option n [ ]
+#    - ok button ---- cancle button -
+# post poll page:
+#    - vote counted!
+#    - if openresults link to results page -
+# results-page
+#    - option 1 [count] percent
+#    - option n [count] percent
