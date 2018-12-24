73ad5007dba465fc0b2ff0970e7614ad1af2d4b4
Yannik Schmidt <yannik.schmidt@thermoscan.de>
2018-12-24 20:51:32 +0100

    flask server

--- server.py
+++ server.py
@@ -1,8 +1,46 @@
-from flask import Flask
-from configparse_wrapper.cpwrap import CFG
+from flask import Flask, requests
+from cpwrap import CFG
 
 app = Flask(CFG("appName"))
 
+def getParam(param):
+    return requests.args.get(param)
+
+########################################################
+
 @app.route('/')
-def main(env, start_response):
+def startPage():
     return "<span style='color:red'>I am app 1</span>"
+
+@app.route('/create')
+def createPoll():
+    poll_name = getParam(dd"name") 
+    return frontend.buildCreatePoll(poll_name)
+
+@app.route('/post-create')
+def postCreatePoll():
+    poll_name = getParam("name") 
+    return frontend.buildPostCreatePoll(poll_name)
+
+@app.route('/ask-token')
+def askToken():
+    poll_name = getParam("name") 
+    return frontend.askToken(poll_name)
+
+@app.route('/vote')
+def voteInPoll():
+    poll_name = getParam("name") 
+    return frontend.buildVoteInPoll(poll_name)
+
+@app.route('/post-vote')
+def postVote():
+    poll_name = getParam("name") 
+    return frontend.buildPostVote(poll_name)
+
+@app.route('/results')
+def showResults():
+    poll_name = getParam("name") 
+    return frontend.buildShowResults(poll_name)
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1')
