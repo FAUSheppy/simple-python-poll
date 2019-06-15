ca51cba2aaa7194fc826d66457e0e67ba84c7eed
Yannik Schmidt <sheppy@atlantishq.de>
2019-06-15 13:03:42 +0200

    [git fast commit] 15. Jun 2019 - 13:03:42

--- htmlsnippets.py
+++ htmlsnippets.py
@@ -1,25 +1,3 @@
-import os
-HTML_DIR = "html_js_partials/"
-
-def readPartial(name):
-    if not name.endswith((".html",".js",".css")):
-        name = name + ".html"
-    path = os.path.join(HTML_DIR, name)
-    with open(path,"r") as f:
-        return f.read()
-
-def buildTokenPartial(tokens):
-    tokenPartial = ""
-    if tokens:
-        for tk in tokens:
-            if type(tk) == tuple:
-                tk = tk[0]
-            tokenPartial += tokenWrapper.format(tk)
-    return tokenPartial
-
-startPage      = readPartial("base").format(title="simple-poll", body=readPartial("start-page"))
-pollCreator    = readPartial("base").format(title="poll-create", body=readPartial("create-poll-partial"))
-
 tokenWrapper   = "<div class='single-token'>{}</div>"
 optionWrapper  = "<span><input class='vote-option' type=checkbox id={}>{}</input></span>"
 noPollSelected = "<h1>No poll selected</h1>"
@@ -32,7 +10,4 @@ resultsHref      = "'/results?name=%s'"
 
 pollHasNoTokens   = "<div class='main-container'><h1>Poll does not use tokens.</h1></div>"
 adminTokenInvalid = "<div class='main-container'><h1>Admin token invalid.</h1></div>"
-
-voteFailed = readPartial("base").format(title="Vote Failed", body=readPartial("vote-failed"))
-
 redirectTo = '<html><meta http-equiv="refresh" content="0"; url="{}"></html>'
