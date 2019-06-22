1bea92ea68fde1cc9125fd9caf40430870485240
Sheppy <sheppy@atlantishq.de>
2019-06-22 23:19:46 +0200

    allow urls in token input

--- static/tokeninput.js
+++ static/tokeninput.js
@@ -1,14 +1,25 @@
 function goButton(){
-    var tokenInput = document.getElementById('input').value
+  var tokenInput = document.getElementById('input').value
+  console.log(tokenInput) 
+  if(tokenInput.includes("token")){
+    /* basicly if someone pasted a vote-url into the token field */
+    try{
+        tokenInput = tokenInput.split("token=")[1].split("&")[0]
+    }catch(error){
+        alert("That does not look like a token.")
+        return
+    }
+  }
+
 	requestURL = window.location.origin + "/checktoken?token=" + tokenInput
 
 	var xhttp = new XMLHttpRequest();
 	xhttp.onload = function() {
 	    if (this.status == 200) {
-            	window.location.href = window.location.origin + xhttp.responseText
+          window.location.href = window.location.origin + xhttp.responseText
 	    }else{
-                alert("Token rejected")
-        }
+          alert("Token rejected")
+      }
 	};
 	xhttp.open("GET", requestURL, true);
 	xhttp.send();
